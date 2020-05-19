ad68f071a2c5c8437bb6c0625b907b4a6d2b68ab
Luigi Cruz <alainluigicruz@gmail.com>
2020-05-19 15:30:19 +0800

    Add relationship between Roles to Permission vice versa

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    /**
+     * Setup relationship between role and premission
+     *
+     * @var array
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_permissions');
+    }
 }
--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * Setup relationship between role and premission
+     *
+     * @var array
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
 }
